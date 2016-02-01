77f9349c2923b63844088fcf82d648a6b8b3f91f
JessicaHerford <jrherford12@gmail.com>
2016-02-01 13:33:42 -0700

    getting June layout

--- toppbox/public/javascripts/vote.js
+++ toppbox/public/javascripts/vote.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
 
 var drake=dragula([document.querySelector(".allPicks"), document.querySelector(".userPick")]);
 
-// $('#submitPics').click(function(){
-//   var contents = $('.userPick').html();
-//   console.log(contents);
-// })
+
 
 });
