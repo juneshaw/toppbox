6a33322eaa97d147e0964e9e5f481c92825c3a60
Corey Yaspan <cspan00@gmail.com>
2016-02-02 06:53:19 -0700

    send contents of userPicks div to '/' post route and log contents

--- toppbox/public/javascripts/vote.js
+++ toppbox/public/javascripts/vote.js
@@ -3,18 +3,14 @@ $(document).ready(function() {
 
 var drake=dragula([document.querySelector(".allPicks"), document.querySelector("#userPick")]);
 
-drake.on('drop',function(el, target){
-   console.log(el.className);
+
+$("#button").click(function(){
+  data = $("#userPick").html();
+  $("#useDataField").val(data);
+  $("#someForm").submit();
 })
-//
-// $('#button').click(function(){
-//   var first = $("#userPick:nth-of-type(1)").html()
-//   var second = $("#userPick:nth-of-type(2)").html()
-//
-//   console.log(first);
-//   console.log(second);
-//   document.getElementById("the-form").value = first;
-// })
+
+
 
 
 });
