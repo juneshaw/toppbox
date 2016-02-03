7562f31a6aeb77b57b4bbfb791aab14e8fe6b4fa
Joshua Sparks <JoshuaTaylorBW@gmail.com>
2016-02-03 15:24:07 -0700

    fixed nav bar so it doesnt think 'home' is a movie

--- toppbox/routes/facebook.js
+++ toppbox/routes/facebook.js
@@ -25,22 +25,24 @@
     })
   })
 
-    router.get('/home/', function(req, res){
+  router.get('/home/home', function(req, res){
+    var movies= []
+    //req.query.option would equal 'my-cool-option'
+    var usersName = localStorage.getItem('name').replace(/['"]+/g, '');
+    var originalUrl = localStorage.getItem('photo');
+    var email = localStorage.getItem('email').replace(/['"]+/g, '');
+    var url = originalUrl.replace(/['"]+/g, '');
 
-      //req.query.option would equal 'my-cool-option'
-      var usersName = localStorage.getItem('name').replace(/['"]+/g, '');
-      var originalUrl = localStorage.getItem('photo');
-      var email = localStorage.getItem('email').replace(/['"]+/g, '');
-      var url = originalUrl.replace(/['"]+/g, '');
-      getupcoming.then(function(data){
-        data['results'].forEach(function(movie){
-          if(movie.poster_path !== null && movie.poster_path !== "" && movie.poster_path !== "null"){
-            movies.push( {image:'https://image.tmdb.org/t/p/w185'+movie.poster_path, title: movie.title})
-          }
-        });
-        res.render("logged-in/index", {movies:movies, photoUrl: url, userName: usersName, toppboxemail:email});
-      })
+    getupcoming.then(function(data){
+      console.log(data);
+      data['results'].forEach(function(movie){
+        if(movie.poster_path !== null && movie.poster_path !== "" && movie.poster_path !== "null"){
+          movies.push( {image:'https://image.tmdb.org/t/p/w185'+movie.poster_path, title: movie.title})
+        }
+      });
+    res.render("logged-in/index", {movies:movies, photoUrl: url, userName: usersName, toppboxemail:email});
     })
+  })
 
 
     // handle the callback after facebook has authenticated the user
