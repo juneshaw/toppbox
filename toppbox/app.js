542c405d241c50b0b2102a20963c7cfb71c472ff
June Shaw <june.shaw@me.com>
2016-02-03 16:31:55 -0700

    heroku config to facebook

--- toppbox/app.js
+++ toppbox/app.js
@@ -53,7 +53,7 @@ app.use('/buildDB', buildDB);
 passport.use(new FacebookStrategy({
     clientID: "1643675802563018",
     clientSecret: "73c625d213a3ef1ae03eb404c7cd1609",
-    callbackURL: "http://localhost:3000/auth/facebook/",
+    callbackURL: "http://toppbox.herokuapp/auth/facebook/",
     enableProof: true,
     profileFields: ['name', 'id','picture.type(large)', 'emails']
   },
