c1d37accf86febae1ad3c5ad741f83772fa1c5e5
June Shaw <june.shaw@me.com>
2016-02-04 14:19:50 -0700

    scoring algorithm done

---
+++ toppbox/public/javascripts/score.js
@@ -0,0 +1,27 @@
+var db = require('../../src/db.js')
+
+
+  function score (user_id, date) {
+    var scoreTotal = 0;
+    db.scoresByDate(date).then(function(scores) {
+      db.votesByUserDate(user_id, date).first().then(function(votes) {
+        db.voteMovies(votes.id).then (function(movieVotes) {
+          console.log('length of scores: ', scores.length)
+          console.log('length of movieVotes: ', movieVotes.length)
+          movieVotes.forEach(function(vote) {
+            scores.forEach(function(score) {
+              if (vote.movie_id === score.movie_id && vote.rank === score.rank) {
+                scoreTotal++;
+              }
+            })
+          })
+          db.updateUserScore(user_id, scoreTotal).then(function(results) {
+
+            console.log('score total', scoreTotal);
+          })
+        })
+      })
+    })
+  }
+  var date = new Date();
+  score(107, date);
