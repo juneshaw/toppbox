ccd9fc63b82fe1e03aedb30536b619f13b89817d
June Shaw <june.shaw@me.com>
2016-02-04 21:42:51 -0700

    fixed vote promise

--- toppbox/public/javascripts/helpers.js
+++ toppbox/public/javascripts/helpers.js
@@ -21,6 +21,7 @@ module.exports = {
                         'rank': index+1,
                       'comment': 'my fave movie ever'}
         db.insertMovieVote(movieVote).then(function(results) {
+          console.log('keep this here to execute the promise');
         });
       })
     })
